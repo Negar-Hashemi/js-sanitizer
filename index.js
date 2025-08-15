7e59ca85402a42def20c8cadba3302a56783dbc0
Negar-Hashemi <76728993+Negar-Hashemi@users.noreply.github.com>
2025-08-15 22:21:29 +1200

    added log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 const sanitizer = require('./sanitizer.js');
-
+console.log(`########### INDEX JS-SANITIZER #################`);
 module.exports = function (babel, options) {
   return sanitizer(babel, options);
 };
